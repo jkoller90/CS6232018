cb1fdf53f0cbf5d28395596bd1c8b33a6d4fde80
Jon Koller <jmk4411@gmail.com>
2018-07-07 14:02:31 -0400

    Update week5hw_cs623_koller.sql

--- week5hw_cs623_koller.sql
+++ week5hw_cs623_koller.sql
@@ -105,20 +105,26 @@ question 9 : Sum of the stocked quantity of each product.
 */
 SELECT prod, SUM(quantity) FROM stock GROUP BY prod;
 
-/*
-question 10 : Products names stocked in at least 3 depots.
-(a) using count
-(b) without using count
-*/
---- a --- 
+/*10. Products names stocked in at least 3 depots.
+(a) using count*/
 
---- b --- 
+SELECT pname FROM Product
+WHERE prod IN 
+(SELECT prod FROM Stock GROUP BY prod HAVING COUNT(dep)>=3);
 
-/*
-question 11 : #prod stocked in all depots.
-(a) using count
-(b) using exists/not exists
+/*(b) without using count*/
 
---- a --- 
+SELECT pname FROM Product WHERE prod IN
+(SELECT S1.prod FROM Stock S1, Stock S2, Stock S3 WHERE
+S1.dep <> S2.dep AND S1.dep <> S3.dep AND S2.dep <> S3.dep AND S1.prod = S2.prod AND S2.prod = S3.prod);
 
---- b --- 
+
+/*11. #prod stocked in all depots.
+(a) using count*/
+
+SELECT prod FROM Stock GROUP BY prod HAVING COUNT(dep)= (SELECT COUNT(*) FROM Depot);
+
+/*(b) using exists/not exists*/
+SELECT prod FROM Product P WHERE EXISTS 
+(SELECT prod FROM Stock S WHERE P.prod = S.prod GROUP BY prod 
+HAVING COUNT(dep) = (SELECT COUNT(*) FROM Depot ));
